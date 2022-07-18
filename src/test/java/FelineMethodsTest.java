89484ca65ce0a4b881f148b9a83858c7fdf17302
p.kychakova <polina.kychakova@outlook.com>
2022-07-18 13:43:42 +0300

    Удалены методы тестирования класса Animal. В тестах для класса Feline с помощью Mockito.when().thenReturn() иммитируется работа метода getFood() класса Animal, который [метод] вызывается в методе eatMeat().

--- src/test/java/FelineMethodsTest.java
+++ src/test/java/FelineMethodsTest.java
@@ -1,20 +1,31 @@
+import com.example.Animal;
 import com.example.Feline;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
+@RunWith(MockitoJUnitRunner.class)
 public class FelineMethodsTest {
 
     Feline feline = new Feline();
+
+    @Mock
+    Animal animal ;
 
     @Test
     public void FelineGetKittensTest(){
         assertEquals(1, feline.getKittens());
     }
 
+    @Test
+    public void FelineGetCountKittensTest(){ assertEquals(2, feline.getKittens(2)); } // не проверяет ошибку в переданных данных, так как метод не предусматривает проверку данных
 
     @Test
     public void FelineGetFamilyTest(){
@@ -23,6 +34,9 @@ public class FelineMethodsTest {
 
     @Test
     public void FelineEatMeatTest() throws Exception {
+        Mockito.when(animal.getFood("Хищник")).thenReturn(List.of("Животные", "Птицы", "Рыба"));
         assertEquals(List.of("Животные", "Птицы", "Рыба"), feline.eatMeat());
     }
+
+
 }
